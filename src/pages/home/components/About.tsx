7100a9c49c9c2d5e3c3ca743d8ff5620e8e187c5
brnc-oliveira <brnc.oliveira@outlook.com>
2024-10-31 12:00:33 -0300

    Fix: about section

--- src/pages/home/components/About.tsx
+++ src/pages/home/components/About.tsx
@@ -1,14 +1,27 @@
-import { about } from "../constants/home.constants";
+import { about } from '../constants/home.constants';
 
 export default function About() {
-    return (
-        <div className="bg-primary flex items-center justify-between gap-3 lg:gap-12 px-2 md:px-10 lg:px-[180px] max-[360px]:py-2 py-8 md:py-14">
-            <img src={about.url} alt={about.alt} className='w-40 md:w-[300px] lg:w-[400px]' />
+ return (
+  <div className='bg-primary flex items-center justify-between gap-3 lg:gap-12 px-2 md:px-10 lg:px-[180px] max-[360px]:py-2 py-8 md:py-14'>
+   <img src={about.url} alt={about.alt} className='w-40 md:w-[300px] lg:w-[400px]' />
 
-            <div className='text-white'>
-                <h1 className='font-bold text-md md:text-3xl lg:text-3xl mb-2'>Quem somos</h1>
-                <p className='text-xxs md:text-lg lg:text-lg mb-1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor.</p>
-            </div>
-        </div>
-    );
+   <div className='text-white'>
+    <h1 className='font-bold text-md md:text-3xl lg:text-3xl mb-2'>Quem somos</h1>
+    <p className='text-xxs md:text-lg lg:text-lg mb-1'>
+     Nossa história começa em 1973, quando Orlando Mendes iniciou sua carreira na Camargo Corrêa.
+     Ele acumulou experiência em diversas empresas renomadas, como Mendes Júnior e Copovel, e
+     trabalhou como engenheiro agrimensor na Prefeitura de Orleans e na Eliane Cerâmica.
+    </p>
+    <p className='text-xxs md:text-lg lg:text-lg my-3'>
+     Durante 30 anos, Orlando serviu na Prefeitura de Jaguaruna e também lecionou na Universidade
+     UNESC de 1987 a 2001.
+    </p>
+    <p className='text-xxs md:text-lg lg:text-lg my-3'>
+     Prestando serviços nas cidades de Braço do Norte, Araranguá, Tubarão e Jaguaruna. Seu
+     compromisso com a excelência e a dedicação à profissão são inspirações que guiam nossa empresa
+     até hoje.
+    </p>
+   </div>
+  </div>
+ );
 }
--- src/pages/home/components/Contact.tsx
+++ src/pages/home/components/Contact.tsx
@@ -1,27 +1,35 @@
-import { contacts } from "../constants/home.constants";
-import ArrowIcon from "../../../../public/assets/contacts/arrow.png"
+import { contacts } from '../constants/home.constants';
+import ArrowIcon from '../../../../public/assets/contacts/arrow.png';
 
 export default function Contact() {
-    return (
-        <div className="bg-primary flex items-center justify-between md:px-[80px] lg:px-[300px] space-x-6 px-2 py-4 md:py-14">
-            <div className='text-white flex flex-col items-center justify-center'>
-                <h1 className='font-bold max-[360px]:text-[11px] text-[13px] md:text-2xl lg:text-3xl'>Entre em contato conosco</h1>
-                <p className='mb-1 text-center max-[360px]:text-xxs text-xs md:text-lg lg:text-lg'>e faça o seu orçamento</p>
-                <img src={ArrowIcon} alt="Seta apontando para a direita" className="w-10 md:w-[70px] lg:w-[80px]" />
-            </div>
+ return (
+  <div className='bg-primary flex items-center justify-between md:px-[80px] lg:px-[300px] space-x-6 px-2 py-4 md:py-14'>
+   <div className='text-white flex flex-col items-center justify-center'>
+    <h1 className='font-bold max-[360px]:text-[11px] text-[12px] md:text-2xl lg:text-3xl'>
+     Entre em contato conosco
+    </h1>
+    <p className='mb-1 text-center max-[360px]:text-xxs text-xs md:text-lg lg:text-lg'>
+     e faça o seu orçamento
+    </p>
+    <img
+     src={ArrowIcon}
+     alt='Seta apontando para a direita'
+     className='w-10 md:w-[70px] lg:w-[80px]'
+    />
+   </div>
 
-            <div>
-                {contacts.map((contact, index) => {
-                    return (
-                        <a href={contact.link} target="_blank" key={index}>
-                            <span className="flex items-center gap-1 mb-1 md:mb-4 lg:mb-6 text-white">
-                                <img src={contact.icon} alt={contact.content} className="max-[360px]:w-5 w-7 md:w-9" />
-                                <h3 className="text-xxs md:text-lg">{contact.content}</h3>
-                            </span>
-                        </a>
-                    );
-                })}
-            </div>
-        </div>
-    );
+   <div>
+    {contacts.map((contact, index) => {
+     return (
+      <a href={contact.link} target='_blank' key={index}>
+       <span className='flex items-center gap-1 mb-1 md:mb-4 lg:mb-6 text-white'>
+        <img src={contact.icon} alt={contact.content} className='max-[360px]:w-5 w-7 md:w-9' />
+        <h3 className='text-xxs md:text-lg'>{contact.content}</h3>
+       </span>
+      </a>
+     );
+    })}
+   </div>
+  </div>
+ );
 }
